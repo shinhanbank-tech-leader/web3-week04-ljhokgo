04e1f9839ce58cd0840714ccb1664e4ff3ce7395
Jaehun Lee <ljhokgo@gmail.com>
2026-03-29 16:51:09 +0900

    Add PureDouble contract to double a value

---
+++ solidity/functions/PureDouble.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    function double(uint _value) external pure returns (uint) {        
+        return _value * 2;
+    }
+}
